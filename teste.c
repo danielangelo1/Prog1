650795fa49895b3fdc1bbe54b1272aa47f3d5394
Daniel Ângelo <84107769+danielangelo1@users.noreply.github.com>
2021-11-21 13:03:12 -0300

    Add files via upload

--- teste.c
+++ teste.c
@@ -1,40 +1,41 @@
 #include<stdio.h>
-/*Você deve fazer programa para receber a quantidade de em reais de dinheiro que uma pessoa que economizou para viajar. Ela vai passar em várias cidades de países diferentes e precisa converter seu dinheiro em dólares, marcos alemães e libra esterlina. O programa deve perguntar ao usuário para qual moeda ele quer converter, fazer a conversão e mostrar ao usuário.*/
+#include<math.h>
+#include<string.h>
+#define tam 101
+
+/*Fazer uma funcao que receba como parametro uma string. Esta
+funcao deve retornar o numero de vogais que a string possui.*/
+
+int contaVogais(char s[]){
+	
+	int numVogais = 0, i, j;
+	char *vogais;
+
+	vogais = "aeiouAEIOU";
+	for(i = 0; s[i]!='\0'; i++){
+		for (j = 0; vogais[j] != '\0'; j++){
+
+			if(s[i] == vogais[j]){
+				numVogais++;
+				break;
+			}
+		}
+	}
+	return numVogais;
+}
+
+
 
 int main(){
-	float real, dolar, marcos, libra;
-	char moeda;
 
-	printf("Insira a quantidade em reais: ");
-	scanf("%f", &real);
+	int valor;
+	char s[tam];
+	printf("Digite uma string: ");
+	fgets(s, tam, stdin);
+	valor = contaVogais(s);
 
-	printf("Deseja converter para qual moeda? \n");
-	printf("1 = dolar \n");
-	printf("2 = marcos alemaes \n");
-	printf("3 = libra \n");
-	scanf("%s", &moeda);
+	printf("Vogais na string = %d", valor);
 
-	switch (moeda)
-	{
-	case '1':
-		dolar = real *5.49;
-		printf("R$%.2f convertidos em dolar = %.2f", real, dolar);
-		break;
-	
-	case '2':
-	marcos = real * 2;
-	printf("R$%.2f convertidos em marcos alemaes = %.2f", real, marcos);
-	break;
-
-	case '3':
-	libra = real *7.36;
-printf("R$%.2f convertidos em libra esterlina = %.2f", real, libra);
-	break;
-
-	default:
-		printf("Moeda digitada invalida");
-		break;
-	}
 
 	return 0;
 }
